55c12bd34881e356e024db4c52c9a5ef6842b4b6
SamodelkinDA <samodelkin.da@phystech.edu>
2021-11-12 15:30:24 +0300

    написано взаимодействие игрового поля со своими объектами

--- model.py
+++ model.py
@@ -1,26 +1,64 @@
 from constans import *
-
+import random
 
 
 class Fruit():
-    pass
+    def __init__(self, snakepose, snakehead):
+        not_founded = True
+        while not_founded:
+            not_founded = False
+            self.pos = (random.randint(0, FIELD_SIZE_W - 1), random.randint(0, FIELD_SIZE_H - 1))
+            for item in snakepose:
+                if snakepose == self.pos:
+                    not_founded = True
+            if snakehead == self.pos:
+                not_founded = True
+    
+    def get_pos(self):
+        return self.pos
 
 class Snake():
     def __init__(self, x, y):
         self.taile = []
         self.head = (x, y)
         self.speed = (1, 0)
+        self.alive = True
 
-    def move(self):
-        pass
-
+    def move(self, fruit):
+        """ Отвечает за перемещение змеи
+        fruit - положение фрукта на поле
+        """
+        x, y = self.head 
+        Vx, Vy = self.speed
+        if not( 0 <= (x + Vx) < FIELD_SIZE_W and 0 <= (y + Vy) < FIELD_SIZE_H):
+            self.speed = (0, 0)
+            self.alive = False
+            print(x, y)
+        else:
+            for part in self.taile[1:]:
+                if part == (x + Vx, y + Vy):
+                    self.speed = (0, 0)
+                    self.alive = False
+            if self.alive:
+                if fruit != (x + Vx, y + Vy):
+                    self.taile.pop(0)
+                self.taile.append(self.head)
+                self.head = (x + Vx, y + Vy)
+                
     def rot_right(self):
+        """ Поворот змеи направо"""
         Vx, Vy = self.speed
         self.speed = (-Vy, Vx)
 
     def rot_left(self):
+        """ Поворот змеи налево"""
         Vx, Vy = self.speed
         self.speed = (Vy, -Vx)
+
+    def get_pos(self):
+        """ Возвращает положения частей хвоста и головы"""
+        return (self.taile, self.head)
+
 
 if __name__ == "__main__":
     print("This is not main file")
--- snake.py
+++ snake.py
@@ -7,34 +7,62 @@ from model import *
 from graphics import *
 from constans import *
 
-class Player(Snake):
-    pass
+
+class Game_field():
+    def __init__(self):
+        self.snake = Snake(FIELD_SIZE_W // 2, FIELD_SIZE_H // 2)
+        self.fruit = Fruit(*self.snake.get_pos())
+        self.screen = pygame.Surface((WIDTH / 2, HEIGHT - BAR_HEIGHT))
+        self.interf = pygame.Surface((WIDTH / 2, BAR_HEIGHT))
+
+    def update(self):
+        self.snake.move(self.fruit.get_pos())
+
+    def new_fruit(self):
+        self.fruit = Fruit(*self.snake.get_pos())
+
+
 
 class Game():
     def __init__(self):
-        self.WIDTH = WIDTH
-        self.HEIGHT = HEIGHT
         self.clock = pygame.time.Clock()
         self.display = pygame.display.set_mode((WIDTH, HEIGHT))
         self.mainloop()
+        self.GAME_RUNNING = True
         #self.start_menu()
 
     def start_menu(self):
         pass
 
     def mainloop(self):
-        cells_1 = []
-        snake1 = Snake(12, 12)
-        while GAME_RUNNING:
+        self.game_fields = []
+        self.game_fields.append(Game_field())
+        while self.GAME_RUNNING:
             self.clock.tick(FPS)
             self.display.fill((0, 0, 0))
             self.keys_loop()
-            draw_field()
-            draw_interface()
+            self.display.blit(draw_field(self.screen, ), (0, BAR_HEIGHT))
+            self.display.blit(draw_interface(self.interf, ), (0, 0))
             pygame.display.flip()
 
     def keys_loop(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.GAME_RUNNUNG = False 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    pause = True
+                    while pause:
+                        self.clock.tick(FPS)
+                        for eventpause in pygame.event.get():
+                            if eventpause.type == pygame.KEYDOWN:
+                                if eventpause.key == pygame.K_p:
+                                    pause = False
+                            if eventpause.type == pygame.QUIT:
+                                self.GAME_RUNNUNG = False 
+                                pause = False
+                if event.key == pygame.K_UP:
+                    pass
 
 
 
