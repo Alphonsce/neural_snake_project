da7d70d0f5f504cfdcb4fc38f366f2f4f056224d
Alphonsce <varlamov.al@phystech.edu>
2021-11-12 21:35:05 +0300

    начинаю писать нейронную сеть

---
+++ for_AI_snake/AI_constans.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+GRAY = (150, 150, 150)
+GREEN = (0, 200, 0)
+WHITE = (255, 255, 255)
+RED = (250, 0, 0)
+BLUE = (0, 0, 250)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+ORANGE = 0xBE5504
+DARK_ORANGE = 0x7A3803
+SNAKE_COLORS = [BLUE, GREEN, YELLOW]
+
+# Стиль текста
+TEXT_FONT = "arial"
+
+WIDTH = 750
+HEIGHT = 800
+
+BAR_HEIGHT = 50
+CELL_SIDE = 30
+WIDTH_OF_TAIL = 0.8 #коэффициент уменьшения для рисовки хвоста
+
+FIELD_SIZE_W = int((WIDTH) // CELL_SIDE)
+FIELD_SIZE_H = int((HEIGHT - BAR_HEIGHT) // CELL_SIDE)
+
+FPS = 30
+FRAMES_PER_STEP = 5
+
+
+
+class Cell(Enum):
+    Empty = 0
+    Snake = 1
+    Snake_head = 2
+    Fruit = 3
