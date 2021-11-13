64812bcf76578dae0595ff3dc8535d136ed23e97
Alphonsce <varlamov.al@phystech.edu>
2021-11-13 21:57:05 +0300

    requirements update and agent starting

--- for_AI_snake/agent_for_learning.py
+++ for_AI_snake/agent_for_learning.py
@@ -1,2 +1,10 @@
-# Здесь будет код для агента для обучения с подкреплением.
-# агент здесь обучается, здесь происходит model.predict(), model.train()
+import torch
+import numpy as np
+import random
+from collections import deque
+from AI_snake import Game_field, Game
+
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
