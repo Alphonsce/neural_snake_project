f920f49abdb51c4e3136d87cc647a6db0d833c0b
Alphonsce <varlamov.al@phystech.edu>
2021-11-21 23:26:19 +0300

    верно определяется state

--- for_AI_snake/agent_for_learning.py
+++ for_AI_snake/agent_for_learning.py
@@ -24,10 +24,62 @@ class Learning_Agent:
         self.trainer = None     # это сам "обучатель" - алгоритм, который обучает нейронную сеть self.model, минимизируя Q функцию принятия решения
 
 
-    def get_state(self, game_class=AI_Game()):
+    def get_state(self, game_class):
         '''метод получения state
-        '''
-        pass
+
+        state - это текущее состояние "среды" агента, в данном случае его полностью
+        можно характеризовать 11 булевыми значениями: есть опасность спереди/слева/справа; направление
+        движения змейки вперед/назад/влево/вправо; фрукт спереди/сзади/слева/справа
+        '''    
+        head_x, head_y = game_class.snake.head
+        fruit_x, fruit_y = game_class.fruit.pos
+
+        right_point = (head_x + 1, head_y)
+        left_point = (head_x - 1, head_y)
+        down_point = (head_x, head_y + 1)
+        up_point = (head_x, head_y - 1)
+
+        moving_left = (game_class.snake.direction == Direction.LEFT)
+        moving_right = (game_class.snake.direction == Direction.RIGHT)
+        moving_up = game_class.snake.direction == Direction.UP
+        moving_down = game_class.snake.direction == Direction.DOWN
+
+        danger_left = ((moving_down and game_class.will_be_dead(right_point)) or 
+            (moving_up and game_class.will_be_dead(left_point)) or
+            (moving_right and game_class.will_be_dead(up_point)) or
+            (moving_left and game_class.will_be_dead(down_point)))
+
+        danger_straight = ((moving_right and game_class.will_be_dead(right_point)) or 
+            (moving_left and game_class.will_be_dead(left_point)) or 
+            (moving_up and game_class.will_be_dead(up_point)) or 
+            (moving_down and game_class.will_be_dead(down_point)))
+
+        danger_right = ((moving_up and game_class.will_be_dead(right_point)) or 
+            (moving_down and game_class.will_be_dead(left_point)) or 
+            (moving_left and game_class.will_be_dead(up_point)) or 
+            (moving_right and game_class.will_be_dead(down_point)))
+
+        food_left = fruit_x < head_x
+        food_right = fruit_x > head_x
+        food_up = fruit_y < head_y
+        food_down = fruit_y > head_y
+
+        state = [
+            danger_left,
+            danger_straight,
+            danger_right,
+
+            moving_left,
+            moving_right,
+            moving_up,
+            moving_down,
+
+            food_left,
+            food_right,
+            food_up,
+            food_down
+        ]
+        return np.array(state, dtype=int)
 
     def add_to_memory(self):
         pass
