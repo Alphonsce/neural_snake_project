8c9282642ffc57333ba4d5d710f6567d800556b8
Alphonsce <varlamov.al@phystech.edu>
2021-11-26 19:33:24 +0300

    агент готов

--- for_AI_snake/agent_for_learning.py
+++ for_AI_snake/agent_for_learning.py
@@ -19,9 +19,9 @@ class Learning_Agent:
     def __init__(self):
         self.number_of_games = 0
         self.memory = deque(maxlen=MAX_MEMORY)      # при переполнении памяти, будет удалять данные из левого края списка
-        self.epsilon = EPSILON
+        self.epsilon = 0
 
-        self.model = None       # нейронная сеть с 11 входными нейронами(state) и 3 выходными (action)
+        self.model = training_model.Neural_network(11, 256, 3)       # нейронная сеть с 11 входными нейронами(state) и 3 выходными (action)
         self.trainer = None     # это сам "обучатель" - алгоритм, который обучает нейронную сеть self.model, минимизируя Q функцию принятия решения
 
 
@@ -83,22 +83,42 @@ class Learning_Agent:
         return np.array(state, dtype=int)
 
     def add_to_memory(self, state, action, reward, next_state, game_over):
-        pass
+        # аппендим один tuple из характеристик на текущем шагу в память
+        self.memory.append((state, action, reward, next_state, game_over))    # memory - это deque с удалением левого края, когда переполнено
 
     def long_memory_train(self):
-        # обучается на всей памяти, которую мы создаем при помощи add_to_memory
-        pass
+        '''обучается на всей памяти(ее части размером в batch), которую мы создаем при помощи add_to_memory'''
+        if len(self.memory) > BATCH_SIZE:
+            part_of_memory = random.sample(self.memory, BATCH_SIZE)     # возвращает список tuple'ов, если в памяти больше чем выбранный batch, то берем случайные элементы из памяти
+        else:
+            part_of_memory = self.memory
+
+        states, actions, rewards, next_states, game_overs = zip(*part_of_memory)
+        self.trainer.train_step(states, actions, rewards, next_states, game_overs)
 
     def short_memory_train(self, state, action, reward, next_state, game_over):
-        # обучение на 1 итерации цикла
-        pass
+        '''обучение на 1 итерации цикла'''
+        self.trainer.train_step(state, action, reward, next_state, game_over)
 
     def get_action(self, state):
         #return [0, 1, 0] если хочется сделать проверку.
-        pass
+        # действия в начале будет случайными, затем с количеством эпох, случайность будет снижаться
+        final_action = [0, 0, 0]
+        self.epsilon = STARTING_EPSILON - self.number_of_games
+        if random.randint(0, 200) < self.epsilon:       # epsilon постепенно уменьшается и случацность постепенно пропадет
+            index_of_action = random.randint(0, 2)
+            final_action[index_of_action] = 1
+        else:
+            tensor_state = torch.tensor(state, dtype=torch.float16)
+            prediction = self.model.predict(tensor_state)   # np.array вида [x y z]
+            index_of_action = torch.argmax(prediction).item()       # получаем индекс макс элемента и из torch.tensor и item() переводит его в int
+            final_action[index_of_action] = 1
+        
+        return final_action
 
 
 
+# Можно потом перенести эту функцию в AI_snake и оттуда инициализировать начало обучения
 def training_process():
     '''Функция, которая запускает само обучение нейронной сети,
     '''
@@ -132,7 +152,7 @@ def training_process():
                 best_score = score
                 # agent.model.save()
             
-            print('game:', agent.number_of_games, 'score:', score, )
+            print('game:', agent.number_of_games, 'score:', score)
 
 if __name__ == '__main__':
     training_process()
